9f974a1fc586c00b822184bcb1a6b8396294383f
Daniele Trapani <daniele.trapani@live.it>
2013-12-27 01:43:15 +0100

    Security and refactoring

--- app.rb
+++ app.rb
@@ -10,18 +10,17 @@ get '/' do
 end
 
 post '/post' do
-  charset = %w{ 2 3 4 6 7 9 a c d e f g h j k m n p q r t v w x y z}
-  token = (0...3).map{ charset.to_a[rand(charset.size)] }.join
-  REDIS.set(token, params[:text])
-  REDIS.expire(token, 600)
+  token = generate_token
+  REDIS.set("text:#{token}", h(params[:text]))
+  REDIS.expire("text:#{token}", 600)
   flash[:success] = "Ok. Scan the QR code or copy the link below to share this item."
   redirect "/#{token}"
 end
 
 get '/:token' do
   token = params[:token]
-  @text = REDIS.get(token)
-  @expire = REDIS.ttl(token)
+  @text = REDIS.get("text:#{token}")
+  @expire = REDIS.ttl("text:#{token}")
   if @text.nil?
     redirect '/'
   end
@@ -29,12 +28,20 @@ get '/:token' do
 end
 
 helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+
   def render(*args)
     if args.first.is_a?(Hash) && args.first.keys.include?(:partial)
       return haml "_#{args.first[:partial]}".to_sym, :layout => false
     else
       super
     end
+  end
+
+  def generate_token(max = 3)
+    charset = %w{ 2 3 4 6 7 9 a c d e f g h j k m n p q r t v w x y z}
+    token = (0...max).map{ charset.to_a[rand(charset.size)] }.join
   end
 end
 
